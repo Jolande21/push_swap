37048a8555c3802b13f25a7d2d82178a464a470a
Jolande Steenput <jsteenpu@mi-r3-p7.s19.be>
2023-09-18 15:08:23 +0200

    cleaning index.c file

--- srcs/index.c
+++ srcs/index.c
@@ -6,7 +6,7 @@
 /*   By: jsteenpu <jsteenpu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 11:12:00 by jsteenpu          #+#    #+#             */
-/*   Updated: 2023/09/18 14:55:47 by jsteenpu         ###   ########.fr       */
+/*   Updated: 2023/09/18 15:07:30 by jsteenpu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 int get_minimum(t_stack **a)
 {
     t_stack	*head;
-    int		min_value; // Initialize to maximum possible value
+    int		min_value;
 
 	head = *a;
 	min_value = INT_MAX;
     while (head)
     {
         if (head->value < min_value && head->index == -1)
-        {
             min_value = head->value;
-        }
         head = head->next;
     }
     return (min_value);
@@ -39,22 +37,15 @@ void	index_stack(t_stack **a)
 
 	head = *a;
 	index = -1;
-	min_value1 = get_minimum(a); // 6
-	min_value2  = min_value1; // 6
+	min_value1 = get_minimum(a); 
+	min_value2  = min_value1; 
     while (head)
     {
-		//printf("the min: %d\n", min_value1);
 		if (head->value == min_value1 && head->index == -1)
 		{
-			// set the correct index
-			//printf("the head value: %d\n", head->value);
 			index++;
-			head->index = index; // 0 -> 1
-			// // get the new minimum value 
-			min_value2 = get_minimum(a); // 38
-			
-			// // init the head back to the beginning of the list
-			//head = *a; // Reset head to the beginning of the list
+			head->index = index;
+			min_value2 = get_minimum(a);
 		}
 		if (min_value1 != min_value2)
 		{
@@ -64,17 +55,10 @@ void	index_stack(t_stack **a)
         else
 			head = head->next;
     }
-	
-	// print the indexed list
-	head = *a;
-	while (head)
-	{
-		printf("%d\t, index: %d\n", head->value, head->index);
-		head = head->next;
-	}
-	printf("\n");
 }
-// de functie sprint direct naar de volgende node ipv eerst node na het zoeken van een nieuw min
+// de functie sprint direct naar de volgende node ipv eerst node na het zoeken van een nieuw min!! 
+// daarom extra controle toegevoegd zodat er kan gecheck worden of er een nieuw minimum gevonden is
+// indien nieuw minimum dan moet er opnieuw van het begin van de linked list check worden
 
 
 
